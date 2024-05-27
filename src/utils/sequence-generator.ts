8a0253b76c3554af8d0edae84580dd5357d43db9
Suman Kisku <sumankisku1@gmail.com>
2024-05-27 13:52:27 +0530

    Changed comments

--- src/utils/sequence-generator.ts
+++ src/utils/sequence-generator.ts
@@ -1,3 +1,8 @@
+/**
+ * Generate A,B,C...Z,AA,AB sequences
+ * @param n take a number for to generate sequence for n
+ * @returns "A" | "B" ... "AA" | "AB" | "ZZ"
+ */
 export const generateSequence = (n: number): string => {
     let result = '';
     while (n > 0) {
