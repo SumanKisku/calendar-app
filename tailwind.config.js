b85d37631cf743d554418758eeb07f8fd3e34cfe
Suman Kisku <sumankisku1@gmail.com>
2024-05-27 14:48:03 +0530

    added custom grid-columns

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,12 +6,7 @@ export default {
   ],
   theme: {
     extend: {
-      gridTemplateColumns: {
-        30: '190px repeat(30, minmax(100px, 1fr))',
-        31: '190px repeat(31, minmax(100px, 1fr))',
-        28: '190px repeat(28, minmax(100px, 1fr))',
-        29: '190px repeat(29, minmax(100px, 1fr))',
-      },
+
     },
   },
   plugins: [],
